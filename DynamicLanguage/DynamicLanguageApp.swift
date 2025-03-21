93099c1bd67dcf7b7a17a6b699a325c455a54a0f
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-03-22 00:57:55 +0100

    Initial Commit

---
+++ DynamicLanguage/DynamicLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicLanguageApp.swift
+//  DynamicLanguage
+//
+//  Created by Javier Calatrava on 22/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamicLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DynamicLanguageTests/DynamicLanguageTests.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicLanguageTests.swift
+//  DynamicLanguageTests
+//
+//  Created by Javier Calatrava on 22/3/25.
+//
+
+import Testing
+@testable import DynamicLanguage
+
+struct DynamicLanguageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
